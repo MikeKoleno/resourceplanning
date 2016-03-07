2d48db0f9738ba71db28138625eb09da9944d750
Purna Annapureddy <pannapureddy@solstice-consulting.com>
2016-03-07 15:39:32 -0600

    Removed unused module dependency

--- src/js/module.js
+++ src/js/module.js
@@ -1,4 +1,4 @@
-angular.module('RDash', ['ui.bootstrap', 'ui.calendar', 'ui.router', 'angular-svg-round-progress', 'ngCookies', 'LocalStorageModule', 'RDash.Services']);
+angular.module('RDash', ['ui.bootstrap', 'ui.router', 'angular-svg-round-progress', 'ngCookies', 'LocalStorageModule', 'RDash.Services']);
 
 //defining services module
 angular.module('RDash.Services', []);
