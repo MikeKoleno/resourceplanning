0517e2266ee317993b108fe790716a48e2f7ab76
Purna Annapureddy <pannapureddy@solstice-consulting.com>
2016-03-09 14:44:33 -0600

    Updated dashboard view with weeks instead of days and sorting with allocation and name properly

--- src/js/controllers/dashboard-ctrl.js
+++ src/js/controllers/dashboard-ctrl.js
@@ -1,45 +1,51 @@
 angular.module("RDash")
-    .controller("DashboardController", ["$scope", "employeeSrv", "projectSrv", "utilitySrv", DashboardController]);
+    .controller("DashboardController", ["$scope", "$filter", "employeeSrv", "projectSrv", "utilitySrv", DashboardController]);
 
-function DashboardController($scope, employeeSrv, projectSrv, utilitySrv) {
+function DashboardController($scope, $filter, employeeSrv, projectSrv, utilitySrv) {
 
     var fetchAllocation = function ($index) {
-        angular.element('.fc-bg table tbody').append("<tr></tr>");
         projectSrv.fetchAllocation($scope.employees[$index].email['S'], function (error, data) {
             if (!error) {
                 var projectsResourceData = data.Items;
-                $scope.employees[$index].allocations = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100];
+                $scope.employees[$index].allocations = [700, 700, 700, 700, 700, 700, 700, 700, 700, 700];
                 if (projectsResourceData.length !== 0) {
                     angular.forEach(projectsResourceData, function (eachProjectResource) {
-                        findAllocationByWeek(eachProjectResource, $index);
+
+                        for (var i = 0; i < $scope.weeks.length; i++) {
+                            findAllocationByWeek(eachProjectResource, $index, $scope.weeks[i]);
+                        }
                     });
                 }
             }
         });
     };
 
-    var findAllocationByWeek = function (projectResource, employeeIndex) {
+    var findAllocationByWeek = function (projectResource, employeeIndex, duration) {
         projectSrv.fetchProjectDetails(projectResource.projectId['N'], function (error, data) {
             if (!error) {
                 var projectDetails = data.Items[0];
                 var endDate = new Date(projectDetails.endDate['S']);
                 endDate.setHours(0, 0, 0, 0);
 
-                var allocationsTemp = $scope.employees[employeeIndex].allocations;
-
-                for (var i = 0; i < $scope.dates.length; i++) {
-                    if (endDate >= $scope.dates[i]) {
-                        allocationsTemp[i] -= parseInt(projectResource.allocation['N']);
+                var allocationIndex = $scope.weeks.indexOf(duration),
+                    allocationsInWeek = $scope.employees[employeeIndex].allocations[allocationIndex],
+                    day = duration[0],
+                    weekIndex = 0;
+                while(day <= duration[6]) {
+                    if (endDate >= day) {
+                        allocationsInWeek -= parseInt(projectResource.allocation['N']);
                     } else {
-                        allocationsTemp[i] -= 0;
+                        allocationsInWeek -= 0;
                     }
+                    weekIndex++;
+                    day = duration[weekIndex];
                 }
+
+                $scope.employees[employeeIndex].allocations[allocationIndex] = angular.copy(allocationsInWeek);
 
                 if (!$scope.$$phase) {
                     $scope.$digest();
                 }
-
-                $scope.employees[employeeIndex].allocations = allocationsTemp;
             }
         });
     };
@@ -48,15 +54,22 @@ function DashboardController($scope, employeeSrv, projectSrv, utilitySrv) {
         employeeSrv.fetch(function (error, data) {
             if (!error) {
                 var employees = data.Items;
-                $scope.employees = [];
                 angular.element('.fc-bg table tbody').html('');
-                var i = 0;
                 angular.forEach(employees, function (employee) {
                     employeeSrv.fetchEmployeeRoles(employee.email['S'], function (error, data) {
                         if (!error && data.Items.length !== 1 || data.Items[0].roles['NS'][0] !== '1') {
-                            $scope.employees.push(employee);
-                            fetchAllocation(i);
-                            i++;
+                            var employeeIndex = -1;
+                            angular.forEach($scope.employees, function(emp, index) {
+                                if (emp.email['S'] === employee.email['S']) {
+                                    employeeIndex = index;
+                                }
+                            });
+                            if (employeeIndex === -1) {
+                                $scope.employees.push(employee);
+                                fetchAllocation($scope.employees.length - 1);
+                            } else {
+                                fetchAllocation(employeeIndex);
+                            }
                         }
                     });
                 });
@@ -65,32 +78,71 @@ function DashboardController($scope, employeeSrv, projectSrv, utilitySrv) {
 
     };
 
-
-    var fetchWeekDays = function () {
-        var curr = new Date();
-        curr.setHours(0, 0, 0, 0);
-        $scope.dates = [curr];
-
-        for (var i = 1; i < 10; i++) {
-            var nextDate = new Date();
-            nextDate = new Date(nextDate.setDate(nextDate.getDate() + i));
-            nextDate.setHours(0, 0, 0, 0);
-
-            $scope.dates.push(nextDate);
+    var getWeekFromDates = function (first, last) {
+        var week = [];
+        var current = angular.copy(first);
+        while (current <= last) {
+            var day = angular.copy(current);
+            week.push(day);
+            current = new Date(current.setDate(current.getDate() + 1));
         }
+        return week;
+    };
 
+    var fetchWeeks = function (first, last, reverseWeek) {
+        var weeks = [];
+        if (!reverseWeek) {
+            for (var i = 1; i <= 10; i++) {
+                weeks.push(getWeekFromDates(first, last));
+                first = new Date(last.setDate(last.getDate() + 1));
+                last = new Date(last.setDate(last.getDate() + 6));
+            }
+            $scope.weeks = weeks;
+        } else {
+            for (var i = 1; i <= 10; i++) {
+                weeks.push(getWeekFromDates(first, last));
+                first = new Date(first.setDate(first.getDate() - 7));
+                last = new Date(last.setDate(last.getDate() - 7));
+            }
+            $scope.weeks = weeks.reverse();
+        }
         fetchEmployees();
+    };
+
+    $scope.showPrevDates = function () {
+        var currDate = $scope.weeks[0][0];
+
+        var first = new Date(currDate.setDate(currDate.getDate() - 7));
+        var last = new Date(currDate.setDate(currDate.getDate() + 6));
+
+        fetchWeeks(first, last, true);
+    };
+
+    $scope.showNextDates = function () {
+        var currDate = $scope.weeks[9][6];
+
+        var first = new Date(currDate.setDate(currDate.getDate() + 1));
+        var last = new Date(currDate.setDate(currDate.getDate() + 6));
+
+        fetchWeeks(first, last, false);
     };
 
     (function () {
         $scope.dashboardSearch = '';
-        $scope.predicate1 = "allocations";
-        $scope.predicate2 = "firstName['S']";
+        $scope.predicate1 = ['-allocations', 'firstName["S"]'];
+        $scope.predicate2 = "";
         $scope.reverse = false;
         $scope.eventSources = [];
         $scope.employees = [];
+        $scope.weeks = [];
 
-        fetchWeekDays();
+        var currDate = new Date();
+        currDate.setHours(0, 0, 0, 0);
+
+        var first = new Date(currDate.setDate(currDate.getDate() - (currDate.getDay() - 1)));
+        var last = new Date(currDate.setDate(currDate.getDate() + 6));
+
+        fetchWeeks(first, last, false);
 
         $scope.$on("refresh", function (event, message) {
             if (message === 'dashboard') {
@@ -100,41 +152,11 @@ function DashboardController($scope, employeeSrv, projectSrv, utilitySrv) {
     })();
 
     $scope.getColorByAllocation = function (allocation) {
-        return utilitySrv.greenToRedColors(10 - (allocation / 10));
+        return utilitySrv.greenToRedColors(10 - Math.floor(Math.floor(allocation) / 10));
     };
 
     $scope.order = function (predicate) {
         $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : false;
         $scope.predicate = predicate;
-    };
-
-    $scope.showPrevDates = function () {
-        var current = new Date();
-        current.setHours(0, 0, 0, 0);
-        if ($scope.dates[0].getTime() != current.getTime()) {
-            var first = $scope.dates[0];
-            $scope.dates = [];
-            for (var i = 9; i >= 0; i--) {
-                var prevDate = angular.copy(first);
-                prevDate = new Date(prevDate.setDate(prevDate.getDate() - i));
-                prevDate.setHours(0, 0, 0, 0);
-
-                $scope.dates.push(prevDate);
-            }
-            fetchEmployees();
-        }
-    };
-
-    $scope.showNextDates = function () {
-        var last = $scope.dates[9]
-        $scope.dates = [];
-        for (var i = 0; i < 10; i++) {
-            var nextDate = angular.copy(last);
-            nextDate = new Date(nextDate.setDate(nextDate.getDate() + i));
-            nextDate.setHours(0, 0, 0, 0);
-
-            $scope.dates.push(nextDate);
-        }
-        fetchEmployees();
     };
 }
